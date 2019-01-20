50c6540785c46a32474f44fa56f1cc3e910f9ab3
jwelliott28 <jwelliott28@gmail.com>
2019-01-20 01:36:20 -0500

    added router variable to test for connectivity

--- pyng.py
+++ pyng.py
@@ -1,8 +1,16 @@
 import os
-hostname = "google.com"
-response = os.system("ping -c 4 " + hostname)
 
-if response == 0:
+router   = "67.129.234.227"
+hostname = "google.com"
+hostname_response = os.system("ping -c 1 " + hostname)
+router_response = os.system("ping -c 1" + router)
+
+if hostname_response == 0:
 	print hostname, 'is up!'
 else:
 	print hostname, 'is down!'
+
+if router_response == 0:
+	print router, 'is up!'
+else:
+	print router, 'is down!'
