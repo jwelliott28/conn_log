71d03d1edbeecac7868de2d46a3395cb3a8b408e
jwelliott28 <jwelliott28@gmail.com>
2019-01-26 23:50:06 -0500

    Moved variables to top of script for cleanliness

--- conn_log.py
+++ conn_log.py
@@ -7,6 +7,15 @@ import urllib2
 import datetime
 import socket
 
+#Name of the daily text log
+file_name = 'Connection_Log_' + str(datetime.date.today()) + '.txt'
+
+#Retrieves current time
+current_time = datetime.datetime.now()
+
+#Creates the daily text log
+todays_log = open(file_name, 'a')
+
 #Function to test for internet signal when called
 #Need to edit function so that it checks/pings router first and then Google. Narrows down troubleshooting and where issue lies
 
@@ -16,15 +25,6 @@ def internet_on():
     #    return True
     except (urllib2.URLError, socket.timeout, KeyboardInterrupt):	
         return False
-
-#Name of the daily text log
-file_name = 'Connection_Log_' + str(datetime.date.today()) + '.txt'
-
-#Creates the daily text log
-todays_log = open(file_name, 'a')
-
-#Retrieves current time
-current_time = datetime.datetime.now()
 
 #If statement that calls internet_on() to see if it returns True
 #Logs the time of disconnect if internet_on() is False
