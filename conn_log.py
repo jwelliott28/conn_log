580935625dfb3d8219c08807d36dabe892c0ec39
jwelliott28 <jwelliott28@gmail.com>
2019-01-30 01:13:40 -0500

    Moved creation and closure of log to the if statement that runs the program

--- conn_log.py
+++ conn_log.py
@@ -1,5 +1,5 @@
 #Jared Elliott
-#Last Updated: 1/16/2019
+#Last Updated: 1/30/2019
 #Connection_Log: Checks for internet connection and logs 
 #		 a disconnect if no signal is detected
 
@@ -12,9 +12,6 @@ file_name = 'Connection_Log_' + str(datetime.date.today()) + '.txt'
 
 #Retrieves current time
 current_time = datetime.datetime.now()
-
-#Creates the daily text log
-todays_log = open(file_name, 'a')
 
 #Function to test for internet signal when called
 #Need to edit function so that it checks/pings router first and then Google. Narrows down troubleshooting and where issue lies
@@ -30,7 +27,8 @@ def internet_on():
 #Logs the time of disconnect if internet_on() is False
 if internet_on() == False:
 
-    todays_log.write('Connection Lost: ' + current_time.strftime('%I:%M %p') + '\n')
-
-#Saves and closes the daily text log after appending the disconnection period
-todays_log.close()
+	 #Creates the daily text log
+	 todays_log = open(file_name, 'a')
+	 todays_log.write('Connection Lost: ' + current_time.strftime('%I:%M %p') + '\n')
+	 #Saves and closes the daily text log after appending the disconnection period
+	 todays_log.close()
